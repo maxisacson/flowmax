8316002ae0b54d5d764367055c38753650951917
max.isacson <trepunktfjorton@gmail.com>
2014-03-26 15:07:17 +0100

    added some functionality

--- flowmax.py
+++ flowmax.py
@@ -1,4 +1,8 @@
-class node:
+class node(object):
 	"""basic node for flowchart"""
-	def __init__(self):
-		self.text = ""
+	def __init__(self, text, x, y):
+		self.text = text
+		self.x = x;
+		self.y = y;
+
+	
