c94f8a2bb4df2c79b88fa875aca027c6fbb7613d
max.isacson <trepunktfjorton@gmail.com>
2014-03-26 16:43:47 +0100

    added some more stuff

--- flowmax.py
+++ flowmax.py
@@ -1,8 +1,23 @@
+
+
+
 class node(object):
 	"""basic node for flowchart"""
-	def __init__(self, text, x, y):
+	nNodes = 0
+	nodes = []
+	width = 0
+	height = 0
+	connectedTo = [];
+	def __init__(self, text, x = 0, y = 0):
 		self.text = text
-		self.x = x;
-		self.y = y;
+		self.x = x
+		self.y = y
+		self.index = node.nNodes
+		node.nNodes+=1
+		node.nodes.append(self)
 
+	def getIndex(self):
+		return self.index
 	
+	def connectTo(self, other):
+		self.connectedTo.append(other.getIndex())
