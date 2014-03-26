fc47224e7798c627215799230fb4112a74b34244
max.isacson <trepunktfjorton@gmail.com>
2014-03-26 17:51:37 +0100

    added connectrion features

--- flowmax.py
+++ flowmax.py
@@ -1,23 +1,50 @@
+validSymbols = ['start', 'query', 'q', 'action', 'a', 'stop']
 
 
+
+def connect(n1, n2):
+	n1.connectTo(n2)
+	n2.connectTo(n1)
+
+def isConnected(n1,n2):
+	if (n1.isConnectedTo(n2) and n2.isConnectedTo(n1)):
+		return True
+	else:
+		return False
 
 class node(object):
 	"""basic node for flowchart"""
-	nNodes = 0
-	nodes = []
-	width = 0
-	height = 0
-	connectedTo = [];
-	def __init__(self, text, x = 0, y = 0):
+	nNodes	=	0
+	nodes		=	[]
+	def __init__(self, text, symbol, x = 0, y = 0):
 		self.text = text
-		self.x = x
-		self.y = y
-		self.index = node.nNodes
-		node.nNodes+=1
+		if (symbol in validSymbols):
+			if (symbol == 'a'):
+				self.symbol = 'action'
+			elif (symbol == 'q'):
+				self.symbol = 'query'
+			else:
+				self.symbol = symbol
+		else:
+			print "*** Symbol '" + symbol + "' is NOT a valid symbol!"
+			exit(-1)
+		self.x				=	x
+		self.y				=	y
+		self.height			=	0
+		self.width			=	0
+		self.connectedTo	=	[]
+		self.index			=	node.nNodes
+		node.nNodes			+=	1
 		node.nodes.append(self)
-
-	def getIndex(self):
-		return self.index
 	
+	def isConnectedTo(self, other):
+		if (other.index in self.connectedTo):
+			return True
+		else:
+			return False
+
 	def connectTo(self, other):
-		self.connectedTo.append(other.getIndex())
+		if (not self.isConnectedTo(other)):
+			self.connectedTo.append(other.index)
+		else:
+			pass
