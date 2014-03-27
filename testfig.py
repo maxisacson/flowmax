a9131508efbf923aee5d48b63558ef7bdf7a86bb
Johan Asplund <asplund@Johans-MacBook-Pro.local>
2014-03-27 14:05:10 +0100

    New distance algorithm + node info flowTo
    I put the nodes further to the right on the x-axis depending on the
    distance. Cleaned up the code a bit, and the new flowTo info about
    nodes, which prevents several arrows going back and forth between the
    same nodes.

--- testfig.py
+++ testfig.py
@@ -3,5 +3,4 @@ import flowmax, tikzcdexport
 flowmax.run("test.fm")
 nodes = flowmax.node.nodes
 
-
 tikzcdexport.makeFig(nodes,"target")
--- tikzcdexport.py
+++ tikzcdexport.py
@@ -7,32 +7,32 @@ def makeFig(nodes,target):
 	#print figdata
 
 	#Finds where to insert code
-	kodindex = figdata.index("%Code goes here\n")+1
+	codei = figdata.index("%Code goes here\n")+1
 
 	#Loop through each node and insert tikz code in .tex file
 	for n in nodes:
 		#Grabbing info about node
-		i = n.index
+		nodei = n.index
 		nodenr = n.label
 		nodetext = n.text
 		nodeshape = n.symbol
-		nodeto = n.connectedToByIndex
+		nodeto = n.flowsToByIndex
+		nodedist = n.dist
 
-		#Just for proof of concept
-		displacement = str(len(nodeto)-1)
-
-		#Create nodes
+		#Create nodes. Sets text size in querys to tiny (otherwise the diamond will probably become huge).
 		if nodeshape == "start" or nodeshape == "stop":
-			figdata.insert(kodindex+i,"\\node[start-stop] at (2*"+displacement+",-2*"+str(i)+") ("+str(nodenr)+") {"+nodetext+"};\n")
+			figdata.insert(codei+nodei,"\\node[start-stop] at (2*"+str(nodedist)+",-2*"+str(nodei)+") ("+str(nodenr)+") {"+nodetext+"};\n")
+		elif nodeshape == "query":
+			figdata.insert(codei+nodei,"\\node["+nodeshape+"] at (2*"+str(nodedist)+",-2*"+str(nodei)+") ("+str(nodenr)+") {\\tiny{"+nodetext+"}};\n")
 		else:
-			figdata.insert(kodindex+i,"\\node["+nodeshape+"] at (2*"+displacement+",-2*"+str(i)+") ("+str(nodenr)+") {"+nodetext+"};\n")	
+			figdata.insert(codei+nodei,"\\node["+nodeshape+"] at (2*"+str(nodedist)+",-2*"+str(nodei)+") ("+str(nodenr)+") {"+nodetext+"};\n")
 
-		#Create arrows
-		if nodeto == None:
-			pass
-		else:
-			for c in nodeto:
-				figdata.insert(kodindex+i+1,"\\draw[->] ("+str(nodenr)+") -- ("+str(c)+");\n")	
+		#Create arrows, checks if there is a flow from a node to itsef
+		for flowto in nodeto:
+			if nodenr == flowto:
+				pass
+			else:
+				figdata.insert(codei+nodei+1,"\\draw[->] ("+str(nodenr)+") -- ("+str(flowto)+");\n")
 			
 
 	#Create/append new target file
@@ -42,4 +42,3 @@ def makeFig(nodes,target):
 	#Write in the target
 	with open(target+".tex","w") as file:
 		file.writelines(figdata)
-		
