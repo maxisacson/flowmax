f09a59743aafa088fc45fa9e31e7b0d37b423e6b
johanasplund <johanasplund@live.se>
2014-06-10 17:02:52 +0200

    Major cleanup of the code, and redesigned the code according to PEP8-rules. Also modified the readme and made so that the .tex-file compiles to a pdf using either xelatex or pdflatex.

--- lib/tikzexport.py
+++ lib/tikzexport.py
@@ -1,34 +1,37 @@
-import flowmax, os
+import flowmax
+import os
 xsize = 4
 ysize = 2
 
-def findHeight(lay,ind):
-	#First check whether ind lies in the list or not
+
+def findHeight(lay, ind):
+	# First check whether ind lies in the list or not
 	try:
 		return lay.index(ind)
 	except ValueError:
 		pass
 
-	for i in xrange(0,len(lay)):
+	for i in range(0, len(lay)):
 		try:
 			lay[i].index(ind)
 		except ValueError:
 			pass
 		else:
-			return [i,lay[i].index(ind)]
+			return [i, lay[i].index(ind)]
 
 
 def getxy(nodes):
 	x = []
 	y = []
-	#Loop through each node
+	# Loop through each node
 	for n in nodes:
-		#Grab some info about each node
+		# Grab some info about each node
 		nodei = n.index
-		nodepos = findHeight(flowmax.layers,nodei)
+		nodepos = findHeight(flowmax.layers, nodei)
 		x.append(xsize*nodepos[1])
 		y.append(-ysize*nodepos[0])
-	return [x,y]
+	return [x, y]
+
 
 def layerSize(layers):
 	sizes = []
@@ -37,14 +40,14 @@ def layerSize(layers):
 	return sizes
 
 
-#Takes list of nodes defined in flowmax.node and produces a target .tex file containing tikz code
-def makeFig(nodes,target):
+# Takes list of nodes defined in flowmax.node and produces
+# a target .tex filecontaining tikz code
+def makeFig(nodes, target):
 	__dir__ = os.path.dirname(os.path.abspath(__file__))
 	__dir__ = __dir__[:-4]
 	filepath = os.path.join(__dir__, "template/template.tex")
-	with open(filepath,"r") as file:
-		figdata = file.readlines()	
-	#print figdata
+	with open(filepath, "r") as file:
+		figdata = file.readlines()
 
 	#Finds where to insert nodes and arrows (arrows must succeed the nodes)
 	nodecodei = figdata.index("%Nodes goes here\n")+1
@@ -62,22 +65,18 @@ def makeFig(nodes,target):
 		nodeshape = n.symbol
 		nodeto = n.flowsToByLabel
 		nodearrowlabel = n.routeLabels
-		
-
-		#Create nodes
-		figdata.insert(nodecodei+nodei,"\\node["+nodeshape+"] at ("+str(xy[0][nodelbl])+","+str(xy[1][nodelbl])+") ("+str(nodelbl)+") {"+nodetext+"};\n")
-
-		#print nodearrowlabel
-		#print nodeto
+		# Create nodes
+		figdata.insert(nodecodei + nodei,
+						"\\node[" + nodeshape + "] at (" + str(xy[0][nodelbl]) +
+						"," + str(xy[1][nodelbl]) + ") (" + str(nodelbl) +
+						") {" + nodetext + "};\n")
 		arlabelstrings = ['']*nodetot
 
 		if len(nodearrowlabel) > 0:
-			for i in xrange(0,len(nodearrowlabel)):
-				arlabelstrings.insert(nodeto[i],nodearrowlabel[i])
+			for i in range(0, len(nodearrowlabel)):
+				arlabelstrings.insert(nodeto[i], nodearrowlabel[i])
 
-		
-
-		#Create arrows
+		# Create arrows
 		for flw in nodeto:
 			diffx = xy[0][nodelbl]-xy[0][flw]
 			diffy = xy[1][nodelbl]-xy[1][flw]
@@ -89,23 +88,52 @@ def makeFig(nodes,target):
 
 			if nodelbl != flw:
 				if (abs(diffy) == ysize or abs(diffx) == xsize):
-					figdata.insert(arri+nodei+1,"\\draw[->] ("+str(nodelbl)+") -- ("+str(flw)+") node [midway,"+fillstring+",tiny] {"+arlabelstrings[flw]+"} ;\n")
+					figdata.insert(arri + nodei + 1,
+									"\\draw[->] (" + str(nodelbl) +
+									") -- (" + str(flw) +
+									") node [midway," + fillstring +
+									",tiny] {" + arlabelstrings[flw] +
+									"} ;\n")
 				elif diffy != 0:
 					if diffx == 0:
-						figdata.insert(arri+nodei+1,"\\draw[->,rounded corners] ("+str(nodelbl)+") -- ("+str(xy[0][nodelbl])+"-"+str(ysize)+","+str(xy[1][nodelbl])+") -- ("+str(xy[0][nodelbl])+"-"+str(ysize)+","+str(xy[1][nodelbl])+"-"+str(diffy)+") node [midway,"+fillstring+",tiny] {"+arlabelstrings[flw]+"} -- ("+str(flw)+");\n")
+						figdata.insert(arri + nodei + 1,
+										"\\draw[->,rounded corners] (" + str(nodelbl) +
+										") -- (" + str(xy[0][nodelbl]) +
+										"-" + str(ysize) + "," + str(xy[1][nodelbl]) +
+										") -- (" + str(xy[0][nodelbl]) +
+										"-" + str(ysize) + "," + str(xy[1][nodelbl]) +
+										"-" + str(diffy) + ") node [midway," + fillstring +
+										",tiny] {" + arlabelstrings[flw] +
+										"} -- (" + str(flw) + ");\n")
 					elif diffx > 0:
-						figdata.insert(arri+nodei+1,"\\draw[->,rounded corners] ("+str(nodelbl)+") -- ("+str(xy[0][nodelbl])+","+str(xy[1][nodelbl])+"-"+str(diffy)+") node [midway,"+fillstring+",tiny] {"+arlabelstrings[flw]+"} -- ("+str(flw)+");\n")
+						figdata.insert(arri + nodei + 1,
+										"\\draw[->,rounded corners] (" + str(nodelbl) +
+										") -- (" + str(xy[0][nodelbl]) +
+										"," + str(xy[1][nodelbl]) +
+										"-" + str(diffy) + ") node [midway," + fillstring +
+										",tiny] {" + arlabelstrings[flw] +
+										"} -- (" + str(flw) + ");\n")
 					else:
-						figdata.insert(arri+nodei+1,"\\draw[->,rounded corners] ("+str(nodelbl)+") -- ("+str(xy[0][nodelbl])+","+str(xy[1][nodelbl])+"-"+str(diffy)+") node [midway,"+fillstring+",tiny] {"+arlabelstrings[flw]+"} -- ("+str(flw)+");\n")
-
-
+						figdata.insert(arri + nodei + 1,
+										"\\draw[->,rounded corners] (" + str(nodelbl) +
+											") -- (" + str(xy[0][nodelbl]) +
+											"," + str(xy[1][nodelbl]) +
+											"-" + str(diffy) + ") node [midway," + fillstring +
+											",tiny] {" + arlabelstrings[flw] +
+											"} -- (" + str(flw) + ");\n")
 	#Create/append new target file
-	tar = open(target+".tex","a")
+	tar = open(target + ".tex", "a")
 	tar.close()
 
 	#Write in the target
-	with open(target+".tex","w") as file:
+	with open(target + ".tex", "w") as file:
 		file.writelines(figdata)
-		print target + ".tex written!"
+		print(target + ".tex written!")
+	try:
+		os.system("xelatex -interaction=nonstopmode " + target + ".tex")
+		os.system("rm *.log *.aux *.tex")
+	except Exception:
+		os.system("pdflatex -interaction=nonstopmode " + target + ".tex")
+		os.system("rm *.log *.aux *.tex")
 
 
