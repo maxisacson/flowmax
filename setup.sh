82f85038ac86a8eb56f3c00b724518ce8c40288e
Max Isacson <trepunktfjorton@gmail.com>
2014-06-20 12:21:30 +0200

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-CURRENTDIR=/Users/asplund/Documents/flowmax
-if test -z `echo $PYTHONPATH | grep -q $CURRENTDIR/lib`; then
-export PYTHONPATH=$PYTHONPATH:$CURRENTDIR/lib
-fi
-if test -z `echo $PATH | grep -q $CURRENTDIR/bin`; then
-export PATH=$PATH:$CURRENTDIR/bin
-fi
