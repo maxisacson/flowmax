75f113731f8edd4579ffdbc865e694612aff38d9
maxisacson <trepunktfjorton@gmail.com>
2014-10-21 15:46:28 +0200

    gave install.sh a makeover

--- install.sh
+++ install.sh
@@ -15,19 +15,21 @@ fi
 
 CURRENTDIR=`pwd`
 
-echo -e "#!$pypath\n\n" >> bin/flowmax
-echo -e "import flowmax, tikzexport, sys" >> bin/flowmax
-echo -e "if len(sys.argv) < 2 or sys.argv[1] == '--help' or sys.argv[1] == '-h':\n" >> bin/flowmax
-echo -e "\tprint \"Usage: \" + sys.argv[0] + \" <infile> [outfile]\"" >> bin/flowmax
-echo -e "\texit(-1)" >> bin/flowmax
-echo -e "if len(sys.argv) < 3:" >> bin/flowmax
-echo -e "\toutfile = \"out\"" >> bin/flowmax
-echo -e "else:" >> bin/flowmax
-echo -e "\toutfile = sys.argv[2]" >> bin/flowmax
-echo -e "infile = sys.argv[1]" >> bin/flowmax
-echo -e "flowmax.run(infile)" >> bin/flowmax
-echo -e "tikzexport.makeFig(flowmax.node.nodes, outfile)" >> bin/flowmax
-echo -e "exit(0)" >> bin/flowmax
+cat <<EOF > bin/flowmax
+#!$pypath
+import flowmax, tikzexport, sys
+if len(sys.argv) < 2 or sys.argv[1] == '--help' or sys.argv[1] == '-h':
+    print "Usage: " + sys.argv[0] + " <infile> [outfile]"
+    exit(-1)
+if len(sys.argv) < 3:
+    outfile = "out"
+else:
+    outfile = sys.argv[2]
+infile = sys.argv[1]
+flowmax.run(infile)
+tikzexport.makeFig(flowmax.node.nodes, outfile)
+exit(0)
+EOF
 
 if test -e bin/flowmax; then
 	chmod 755 bin/flowmax
@@ -40,13 +42,15 @@ if test -e setup.sh; then
 	rm setup.sh
 fi
 
-echo -e "CURRENTDIR=$CURRENTDIR" >> setup.sh
-echo -e "if test -z \`echo \$PYTHONPATH | grep -q \$CURRENTDIR/lib\`; then" >> setup.sh
-echo -e "export PYTHONPATH=\$PYTHONPATH:\$CURRENTDIR/lib" >> setup.sh
-echo -e "fi" >> setup.sh
-echo -e "if test -z \`echo \$PATH | grep -q \$CURRENTDIR/bin\`; then" >> setup.sh
-echo -e "export PATH=\$PATH:\$CURRENTDIR/bin" >> setup.sh
-echo -e "fi" >> setup.sh
+cat <<EOF > setup.sh
+CURRENTDIR=$CURRENTDIR
+if test -z \`echo \$PYTHONPATH | grep -q \$CURRENTDIR/lib\`; then
+export PYTHONPATH=\$PYTHONPATH:\$CURRENTDIR/lib
+fi
+if test -z \`echo \$PATH | grep -q \$CURRENTDIR/bin\`; then
+export PATH=\$PATH:\$CURRENTDIR/bin
+fi
+EOF
 
 echo -e "Installation done. Do \n\t\$ source setup.sh\nto setup your environment.\n\nConsider adding that to your .bashrc ..."
 exit 0
